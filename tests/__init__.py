910dfcc355fd07008764ef55d161f7d24da78233
Garkusha Valery <aston585v@gmail.com>
2024-01-25 20:01:51 +0300

    deleted:    tests/__init__.py

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# print(db_oper.get_list_sites())
-# print(db_oper.check_availability('https://www.psycopg.org'))
-# print(db_oper.check_availability('https://ru.hexlet0.io'))
-# print(db_oper.get_site('https://ru.hexlet1.io'))
